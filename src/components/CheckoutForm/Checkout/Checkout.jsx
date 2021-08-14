30c6e6cd047ecb0161de5fc4c58b34855b60481c
harzjunior <harunjibs@gmail.com>
2021-08-14 16:36:19 +0300

    update

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+//Import the needed stuff from material-UI
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import useStyles from "./styles"; // used material-UI styling for this project
+import PaymentForm from "../PaymentForm";
+import AddressForm from "../AddressForm";
+
+const steps = ["Shipping address", "Payment details"]; // our steps has only 2 steps
+const Checkout = () => {
+  const classes = useStyles();
+
+  const [activeStep, setActiveStep] = useState(0);
+
+  //Form component that becomes active/change depending on the current location
+  // activeStep is equal to 0 then render/show AddresFrom component else render/show PaymentForm
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
+  const Comfirmation = () => {
+    <div>Confirmation</div>;
+  };
+  return (
+    <>
+      {/* padding for navBar */}
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          {/* we used the active step here */}
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {/* mapping through all the steps and we call the steps and map trough it*/}
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {/* if we are on the last step show Confirmation component else show From component*/}
+          {activeStep === steps.length ? <Comfirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
