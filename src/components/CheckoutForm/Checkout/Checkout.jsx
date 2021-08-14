3c518b88076c1a3d1098b2624eecfe2c4fdbcfb8
harzjunior <harunjibs@gmail.com>
2021-08-14 16:51:08 +0300

    update

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -14,19 +14,21 @@ import useStyles from "./styles"; // used material-UI styling for this project
 import PaymentForm from "../PaymentForm";
 import AddressForm from "../AddressForm";
 
-const steps = ["Shipping address", "Payment details"]; // our steps has only 2 steps
+const steps = ["Shipping address", "Payment details"]; // our steps has only 2 steps to reach (from SHIPPING ADDRESS TO PAYMENT DETAILS) in other to complete the form. for example if you had ["Shipping address", "Payment details", "harz", "sweet"] then you have 4 steps to cimplete the form
+
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
   const classes = useStyles();
 
-  const [activeStep, setActiveStep] = useState(0);
+  const Comfirmation = () => {
+    return <div>Confirmation</div>;
+  };
 
   //Form component that becomes active/change depending on the current location
   // activeStep is equal to 0 then render/show AddresFrom component else render/show PaymentForm
   const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
 
-  const Comfirmation = () => {
-    <div>Confirmation</div>;
-  };
   return (
     <>
       {/* padding for navBar */}
